5e8079b96cd88a8ab94738b8ad01acb93b1ae693
mimismosv <oicguevara@gmail.com>
2023-01-23 21:40:05 -0600

    add carrosuel with offer

--- src/views/products.vue
+++ src/views/products.vue
@@ -1,4 +1,31 @@
 <template>
+
+    <div class="container">
+        <div class="carousel carousel-slider">
+            <div class="carousel-item" v-for='productOffer in productsOffer' v-bind:key='productsOffer'>
+
+                <div class="row">
+                    <div class="col s12 m7">
+                    <div class="card small">
+                        <div class="card-image">
+                            <img :src="productOffer.picture">
+                        <span class="card-title">
+                                <span style="text-decoration:line-through;">                               
+                                    Old Price:${{productOffer.price}}
+                                </span>
+                        </span>
+                        </div>
+                        <span class="card-title black-text text-darken-7">Promotion Price: ${{productOffer.promotion_percentage}}</span>
+                        <br>
+                            <a href="#">{{productOffer.name}} [+]More..</a> 
+                    </div>
+                    </div>
+                </div>
+
+            </div>        
+        </div>
+    </div>
+
     <div><h3>Productos</h3></div>
 
     <div class="container">
@@ -16,8 +43,8 @@
                             </div>
                         <div class="card-content">
                         <p>
-                          <div v-if="product.description.length<75">{{product.description}}</div>
-                           <div v-else>{{product.description.substring(0,75)}} <a href="#">[+]More..</a></div>
+                            <div v-if="product.description.length<50">{{product.description}}</div>
+                            <div v-else>{{product.description.substring(0,50)}} <a href="#">[+]More..</a></div>
                         </p>
                         </div>
                         <div class="card-action">
@@ -41,24 +68,31 @@
     data() {
         return {
         products: [],
+        productsOffer: [],
         categories: [],
         }
     },
     async mounted() 
     {
-        console.log(this.$route.query.test)
-        const headers = {
-        tenant: "foo",
-        Authorization: localStorage.getItem('token'),
-        };
-
         await this.axios.get("/products").then((Response) => {
             this.products = Response.data.data;
             //console.log(this.Products);
         }).catch((error) => {
-          var data = error.Response.data;
-          M.toast({ html: data.message });
+            var data = error.Response.data;
+            M.toast({ html: data.message });
         });
-    }    
+
+        await this.axios.get("/promotions").then((Response) => {
+            this.productsOffer = Response.data.data;
+            //console.log(this.Products);
+        }).catch((error) => {
+            var data = error.Response.data;
+            M.toast({ html: data.message });
+        });
+
+
+        var elem = document.querySelector('.carousel')
+        var t = new M.Carousel(elem, null)
+    }, 
     };
 </script>
